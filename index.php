72423b79849eee8c5e82822d2f0bca356d74ac13
Svetoslav Vladov <svetoslavladov@gmail.com>
2018-03-08 00:05:45 +0200

    Front-end part ONE - CSS: fornt-end layout, images:background and logo

--- index.php
+++ index.php
@@ -5,9 +5,23 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./assets/css/main.css">
     <title>Dota Heroes viewer</title>
 </head>
 <body>
+    <div id="wrap">
+        <div id="head">
+            <div id="logo"><a href="./index.php"><img src="./assets/img/logo.png" alt="Website logo"></a></div>
+        </div>
+        <div id="search">
+            <input type="text" name="charName" id="charName" placeholder="Type dota 2 hero name...">
+            <button id="deepSearch">Search</button>
+            <div id="barResult"></div>
+        </div>
+        <div id="content">
 
+        </div>
+    </div>
+    <script src="./assets/js/main.js"></script>
 </body>
 </html>
