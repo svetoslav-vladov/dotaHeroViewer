0aa1e833c18ba004b2f99aa590277827db1eb37e
Svetoslav Vladov <svetoslavladov@gmail.com>
2018-03-08 05:03:07 +0200

    PHP and JS logic to pull and format data, some markup and css

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,33 +2,89 @@
 var load = document.getElementById("loading");
 document.getElementById("charName").addEventListener("input", function(){
     if(this.value == ""){
+        document.getElementById("barResult").innerHTML = "";
         return;
     }
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "logic.php?search=" + this.value);
-    xhr.onprogress = function () {
-        load = document.getElementById("loading");
-        var img = document.createElement("img");
-        load.innerHTML = "";
 
-        img.src = "./assets/img/loading.gif";
-        load.appendChild(img);
-    };
+    load = document.getElementById("loading");
+    var img = document.createElement("img");
+    load.innerHTML = "";
+
+    img.src = "./assets/img/loading.gif";
+    load.appendChild(img);
+
     xhr.onload = function (ev) {
       var heroes = JSON.parse(this.responseText);
 
       var div = document.getElementById("barResult");
       var ul = document.createElement("ul");
+      ul.setAttribute("id","list");
       div.innerHTML = "";
       load.innerHTML = "";
       for(var i = 0; i < heroes.length; i++){
           var li = document.createElement("li");
-          li.innerHTML = heroes[i];
+          li.setAttribute("id",heroes[i]["id"]);
+          li.className = "item";
+          var a = document.createElement("a");
+          var img = document.createElement("img");
+          img.src = heroes[i]["icon"];
+
+          li.innerHTML = heroes[i]["name"];
+          li.appendChild(img);
           ul.appendChild(li);
       }
       div.appendChild(ul);
 
+
+        document.getElementById("list").addEventListener("click",function(e) {
+
+            if(e.target && e.target.nodeName == "LI") {
+
+                console.log(e.target.id + " was clicked");
+
+                var xh = new XMLHttpRequest();
+                xh.open("GET", "logic.php?id=" + e.target.id);
+
+                xh.onload = function () {
+                    document.getElementById("content").innerHTML = "";
+
+                    var charInfo = JSON.parse(this.responseText);
+                    var content = document.getElementById("content");
+                    var divv = document.createElement("div");
+
+
+                        divv.setAttribute("class","hero-"+charInfo["id"]);
+                        content.innerHTML += "<div class='hero-img'><img src='" + charInfo["img"] + "' alt=''></div>";
+                        content.innerHTML += "<div class='hero-name'><span>Name: </span>" + charInfo["name"] + "</div>";
+                        content.innerHTML += "<div class='hero-health'><span>Health: </span>" + charInfo["base_health"] + "</div>";
+                        content.innerHTML += "<div class='hero-mana'><span>Mana: </span>" + charInfo["base_mana"] + "</div>";
+                        content.innerHTML += "<div class='hero-hregen'><span>Health Regen: </span>" + charInfo["base_health_regen"] + "</div>";
+                        content.innerHTML += "<div class='hero-mregen'><span>Mana Regen: </span>" + charInfo["base_mana_regen"] + "</div>";
+                        content.innerHTML += "<div class='hero-att-range'><span>Range: </span>" + charInfo["attack_range"] + "</div>";
+                        content.innerHTML += "<div class='hero-proj-speeed'><span>Projectile speed: </span>" + charInfo["projectile_speed"] + "</div>";
+                        content.innerHTML += "<div class='hero-att-speed'><span>Attack speed: </span>" + charInfo["attack_rate"] + "</div>";
+                        content.innerHTML += "<div class='hero-move-speed'><span>Move speed: </span>" + charInfo["move_speed"] + "</div>";
+                        content.innerHTML += "<div class='hero-turn-rate'><span>Turn speed: </span>" + charInfo["turn_rate"] + "</div>";
+                        content.innerHTML += "<div class='hero-armor'><span>Armor: </span>" + charInfo["base_armor"] + "</div>";
+                        content.innerHTML += "<div class='hero-main-attr'><span>Primary attr: </span>" + charInfo["primary_attr"] + "</div>";
+
+
+
+                };
+
+
+                xh.send();
+
+            }
+        });
+
     };
     xhr.send();
 });
+
+//getting id from the links
+
+
 
--- logic.php
+++ logic.php
@@ -6,14 +6,53 @@ if(isset($_GET["search"])){
     $search = htmlentities($_GET["search"]);
     $result = array();
     $idx = 0;
+    $found = false;
+
     foreach ($heroDB as $hero){
+
         if(strstr(strtolower($hero["localized_name"]), strtolower($search))){
-            $result[] = $hero["localized_name"];
+            $result[] = ["name"=>$hero["localized_name"],"id"=>$hero["id"],"icon"=>$apiHost.$hero["icon"]];
+            $found = true;
             $idx++;
         }
-        if($idx == 7){
+        if($idx == 6){
             break;
         }
     }
-    echo json_encode($result);
+    if(!$found){
+        echo "Hero not found";
+    }
+    else{
+        echo json_encode($result);
+    }
+
+}
+
+if(isset($_GET["id"])){
+    $id = htmlentities($_GET["id"]);
+    $character = array();
+
+    foreach ($heroDB as $key => $hero){
+        if($hero["id"] == $id){
+            $character["id"] = $hero["id"];
+            $character["name"] = $hero["localized_name"];
+            $character["primary_attr"] = $hero["primary_attr"];
+            $character["attack_type"] = $hero["attack_type"];
+            $character["img"] = $apiHost . $hero["img"];
+            $character["icon"] = $apiHost . $hero["icon"];
+            $character["base_health"] = $hero["base_health"];
+            $character["base_mana"] = $hero["base_mana"];
+            $character["base_health_regen"] = $hero["base_health_regen"];
+            $character["base_mana_regen"] = $hero["base_mana_regen"];
+            $character["attack_range"] = $hero["attack_range"];
+            $character["projectile_speed"] = $hero["projectile_speed"];
+            $character["attack_rate"] = $hero["attack_rate"];
+            $character["move_speed"] = $hero["move_speed"];
+            $character["turn_rate"] = $hero["turn_rate"];
+            $character["base_armor"] = $hero["base_armor"];
+            break;
+        }
+
+    }
+    echo json_encode($character);
 }
